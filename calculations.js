58696b2c93935a63ebeb8568cfc03abb2c85c5fd
sara <sara@oak-fern.com>
2023-08-13 14:48:47 -0700

    Add operator function

--- calculations.js
+++ calculations.js
@@ -1,3 +1,4 @@
+//Basic math operator functions
 const add = function(num1, num2) {
     return num1+num2
 }
@@ -12,4 +13,27 @@ const multiply = function(num1, num2) {
 
 const divide = function(num1, num2) {
     return num1/num2
+}
+
+//Add variables for num1, num2, and operator
+let first, second, operator;
+
+//Function for operator
+const operatorFunc = function(num1, op, num2) {
+    switch(op) {
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return subtract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break;
+        default:
+            return "No operator detected";
+    }
 }
